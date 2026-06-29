0fe02e71026023faad431415c46bfd2df2dc5755
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-06-29 14:10:33 -0400

    Fixed rendering

--- Sources/JunkbotApp/Renderer.swift
+++ Sources/JunkbotApp/Renderer.swift
@@ -9,24 +9,43 @@ public struct Renderer {
     }
 
     public static func renderFrame(engine: GameEngine) {
+        // Build render list (exclude removed and level-bounds entities)
+        var list = engine.entities.filter { !$0.removeBeforeRender && $0.type != .levelBounds }
+
+        // Sort descending by Y: higher Y (lower on screen) drawn first (behind)
+        list.sort { $0.y > $1.y }
+
+        // Bubble sort correction matching original game logic:
+        // swap adjacent pair when a is entirely above b, or b is entirely left of a
+        var n = list.count
+        repeat {
+            var newN = 0
+            for i in 1..<n {
+                let a = list[i - 1]
+                let b = list[i]
+                if a.y + a.height < b.y || b.x + b.width <= a.x {
+                    list.swapAt(i - 1, i)
+                    newN = i
+                }
+            }
+            n = newN
+        } while n > 1
+
+        for e in list {
+            drawEntity(e)
+        }
+
+        // Wind, laser beams, teleport effects always drawn on top of entities
         for w in engine.wind {
             let fan = engine.entities[w.fanEntityIndex]
             for i in 0..<w.numExtents {
                 _ = js.js_draw_wind_column?(fan.x + Int32(i) * CELL_W, fan.y, w.extent(at: i), 0)
             }
         }
-
-        for e in engine.entities {
-            if e.removeBeforeRender { continue }
-            if e.type == .levelBounds { continue }
-            drawEntity(e)
-        }
-
         for l in engine.laserBeams {
             let e = engine.entities[l.laserEntityIndex]
             _ = js.js_draw_laser_beam?(e.x, e.y, e.facing, l.extent, 0, 0)
         }
-
         for t in engine.teleportEffects {
             _ = js.js_draw_teleport_effect?(t.x, t.y, t.frameIndex)
         }
@@ -38,12 +57,12 @@ public struct Renderer {
             _ = js.js_draw_brick?(e.x, e.y, e.colorIndex, e.widthInStuds, e.fixed ? 1 : 0)
         case .junkbot:
             var flags: Int32 = 0
-            if e.armored       { flags |= 1 }
-            if e.dead          { flags |= 2 }
+            if e.armored        { flags |= 1 }
+            if e.dead           { flags |= 2 }
             if e.dyingFromWater { flags |= 4 }
-            if e.collectingBin { flags |= 8 }
-            if e.losingShield  { flags |= 32 }
-            if e.gettingShield { flags |= 64 }
+            if e.collectingBin  { flags |= 8 }
+            if e.losingShield   { flags |= 32 }
+            if e.gettingShield  { flags |= 64 }
             _ = js.js_draw_junkbot?(e.x, e.y, e.facing, e.animationFrame, flags)
         case .gearbot:
             _ = js.js_draw_gearbot?(e.x, e.y, e.facing, e.animationFrame)
