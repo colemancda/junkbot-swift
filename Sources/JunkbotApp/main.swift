b5e2fdf718eee0b5d3c113b23f763f38bddacb5b
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-06-29 23:05:18 -0400

    Load WASM instead of JS

--- Sources/JunkbotApp/main.swift
+++ Sources/JunkbotApp/main.swift
@@ -3,9 +3,8 @@ import JavaScriptKit
 #if os(WASI)
   nonisolated(unsafe) let window = JSObject.global
 
-  if let objectConstructor = JSObject.global.Object.function,
-    let exports = objectConstructor.new().object
-  {
+  if let objectConstructor = JSObject.global.Object.function {
+    let exports = objectConstructor.new()
     window.JunkbotWasm = exports.jsValue
   }
 #endif
