8d430727a979da491b08e67f6358c1c832c53b14
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-06-30 00:03:40 -0400

    Update minimum macOS version

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
   name: "Junkbot",
-  platforms: [.macOS(.v13)],
+  platforms: [.macOS(.v14)],
   dependencies: [
     .package(url: "https://github.com/swiftwasm/JavaScriptKit.git", from: "0.56.0")
   ],
