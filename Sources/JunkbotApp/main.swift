0e1f8a97ccba5a85f869c76ae8ca9a7c183e0e33
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-07-01 01:40:38 -0400

    Load JS from Swift

--- Sources/JunkbotApp/main.swift
+++ Sources/JunkbotApp/main.swift
@@ -4,7 +4,7 @@ let window = JSObject.global
 let document = window.document.object!
 
 let script = document.createElement!("script").object!
-script.src = "game.js"
+script.src = "src/game.js"
 _ = document.body.object!.appendChild!(script)
 
 print("WASM successfully injected game.js")
